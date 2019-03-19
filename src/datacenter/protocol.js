749807bf608185fa46e66f14aafd5b3c01ffea79
xu.gaoren <xu.gaoren@chinaredstar.com>
2019-03-19 19:08:13 +0800

    修改协议结构

--- src/datacenter/protocol.js
+++ src/datacenter/protocol.js
@@ -1,14 +1,14 @@
 const PROTOCAL_REQUEST = {
-  '/inshn_dtimao/huibao/req/dev_info': '9001', //电梯设备信息接口
-  '/inshn_dtimao/huibao/req/dev_onoff_info': '9002', // 每月设备离线信息获取接口
-  '/inshn_dtimao/huibao/req/dev_onoff_num': '9003', // 每月设备离线数量获取接口
-  '/inshn_dtimao/huibao/req/dev_alarm_repeat': '9004', // 故障接警未处理接口
-  '/inshn_dtimao/huibao/req/month_dev_num': '9005', // 电梯安装数量获取
-  '/inshn_dtimao/huibao/req/month_dev_status': '9006', // 电梯总在线、离线数获取
-  '/inshn_dtimao/huibao/req/dev_alarm_num': '9007', // 电梯故障数量查看接口
-  '/inshn_dtimao/huibao/req/mai_dev_num': '9008', // 每月维保审核数目接口
-  '/inshn_dtimao/huibao/req/mai_user_info': ' 9009', // 维保人员信息接口
-  '/inshn_dtimao/huibao/req/mai_dev_info': '9010', // 维保记录查询接口
+  '9001':'/inshn_dtimao/huibao/req/dev_info', //电梯设备信息接口
+  '9002':'/inshn_dtimao/huibao/req/dev_onoff_info', // 每月设备离线信息获取接口
+  '9003':'/inshn_dtimao/huibao/req/dev_onoff_num', // 每月设备离线数量获取接口
+  '9004':'/inshn_dtimao/huibao/req/dev_alarm_repeat', // 故障接警未处理接口
+  '9005':'/inshn_dtimao/huibao/req/month_dev_num', // 电梯安装数量获取
+  '9006':'/inshn_dtimao/huibao/req/month_dev_status', // 电梯总在线、离线数获取
+  '9007':'/inshn_dtimao/huibao/req/dev_alarm_num', // 电梯故障数量查看接口
+  '9008':'/inshn_dtimao/huibao/req/mai_dev_num', // 每月维保审核数目接口
+  '9009':'/inshn_dtimao/huibao/req/mai_user_info', // 维保人员信息接口
+  '9010':'/inshn_dtimao/huibao/req/mai_dev_info', // 维保记录查询接口
 };
 
 const PROTOCAL_MAP = [];
