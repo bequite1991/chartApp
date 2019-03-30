53baa304e071d80264e40a4fa696f9a66e9e8d79
minjie.wu <minjie.wu@chinaredstar.com>
2019-03-30 10:28:30 +0800

    消除请求

--- src/models/user.js
+++ src/models/user.js
@@ -10,18 +10,18 @@ export default {
 
   effects: {
     *fetch(_, { call, put }) {
-      const response = yield call(queryUsers);
-      yield put({
-        type: 'save',
-        payload: response,
-      });
+      // const response = yield call(queryUsers);
+      // yield put({
+      //   type: 'save',
+      //   payload: response,
+      // });
     },
     *fetchCurrent(_, { call, put }) {
-      const response = yield call(queryCurrent);
-      yield put({
-        type: 'saveCurrentUser',
-        payload: response,
-      });
+      // const response = yield call(queryCurrent);
+      // yield put({
+      //   type: 'saveCurrentUser',
+      //   payload: response,
+      // });
     },
   },
 
